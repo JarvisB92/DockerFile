d9e10e50e5d026026016213dc009b21acc61cb74
Jarvis Bryant <98724740+JarvisB92@users.noreply.github.com>
2022-05-30 21:41:49 -0500

    Create 'index.html'.py

---
+++ 'index.html'.py
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+        <head>
+        </head>
+        <body>
+                <p>Type #toolboxskills in LinkedIn to see more Tech Content from Jarvis Bryant!!<p>
+
+                 <p>Current Date and Time is: <span id='date-time'></span></p>
+
+        <script>
+                        var dateAndTime = new Date();
+        document.getElementById('date-time').innerHTML=dateAndTime.toLocaleString();
+        </script>
+            </body>
+</html>
