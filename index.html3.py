d1140e61a021efbe60a80ed33a0846d6eecd1e69
Jarvis Bryant <98724740+JarvisB92@users.noreply.github.com>
2022-06-01 01:53:33 -0500

    Create index.html3.py

---
+++ index.html3.py
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+  <head>
+  </head>
+  <body>
+    <p>Type #toolboxskills in LinkedIn to see more Tech Content from Jarvis Bryant!!</p>
+
+    <p>Current Date and Time is: <span id='date-time'></span></p>
+
+    <script>
+        var dateAndTime = new Date();
+        document.getElementById('date-time').innerHTML=dateAndTime.toLocaleString();
+        </script>
+
+    <p>This Nginx docker container has been created by Jarvis Bryant</p>
+    <p>This container image was created at (CDT):</p>
+    
+    </body>
+</html>
